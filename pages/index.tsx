a0c982aa72a8725bc22481c4a4da3e76c29573cd
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-29 15:46:19 +0200

    build : custom document and static files added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from "next";
 
 import { StyledContainer } from "../components/styles/StyledContainer";
 import Header from "../components/Header";
-
+import Head from "next/head";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Buddle</title>
+      </Head>
       <Header title='header' />
       <StyledContainer>
         <h1>Hello Styled Container</h1>
