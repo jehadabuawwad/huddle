8e82352944ed139690499cab37fd944b81288ea3
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-30 15:51:38 +0200

    feat : data retrived to redux state from special hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
+import { useSelector, RootStateOrAny } from "react-redux";
+import { useEffect } from "react";
 import type { NextPage } from "next";
+import Head from "next/head";
+
+import useResource from "../hooks/getResource";
 
 import { Container } from "../components/styles/Container";
 import Header from "../components/Header";
-import Head from "next/head";
+
 const Home: NextPage = () => {
+  const data = useSelector((state: RootStateOrAny) => state.data);
+  const { getResource } = useResource();
+
+  useEffect(() => {
+    getResource();
+  }, [getResource]);
+
   return (
     <>
       <Head>
         <title>Buddle</title>
       </Head>
       <Header title='header' />
-      <Container title="body-container">
+      <Container title='body-container'>
         <h1>Hello Styled Container</h1>
       </Container>
     </>
